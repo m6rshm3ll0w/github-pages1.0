aa413c5c46439a6975a3659bb28869e942f35693
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2024-08-29 20:14:20 +0300

    sc

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(),preact(), tailwind()],
 });
