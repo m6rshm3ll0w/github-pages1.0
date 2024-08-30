b8d5a4fbea8d5e0b851a4397be02e5e5a85b07bd
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2024-08-30 16:05:13 +0300

    rename

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "M6rshm3ll0w's Blog";
+export const SITE_TITLE = "M1K0 Blog";
 export const SITE_DESCRIPTION = 'Welcome to my website!';
