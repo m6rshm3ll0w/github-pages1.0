f5d3f4b717f831fbe3bd096e52e50dea76370a54
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2024-08-31 12:45:51 +0300

    kk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 
 import sitemap from '@astrojs/sitemap';
 
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
